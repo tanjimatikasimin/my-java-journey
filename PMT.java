f4d3f94c89ffd5f209dd5349423a61f5afb3ef75
tanjimatikasimin <tanjimatikasiminnn@gmail.com>
2026-02-05 00:45:46 +0600

    print multiplication table

---
+++ PMT.java
@@ -0,0 +1,9 @@
+
+public class PMT {
+    public static void main(String[] args) {
+        int number = 5;
+        for(int i=0; i<=10; i++){
+            System.out.println(number + "*" + i + "=" + (number*i));
+        }
+    }
+}
