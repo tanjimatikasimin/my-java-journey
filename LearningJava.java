f0b128779ddd0f4038163f49c4e8377ae3ea7b79
tanjimatikasimin <tanjimatikasiminnn@gmail.com>
2026-02-05 00:15:06 +0600

    taking input and showing output

--- LearningJava.java
+++ LearningJava.java
@@ -1,5 +1,13 @@
+
+import java.util.*;
+
 public class LearningJava {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.println("meow meow");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print(name);
+
     }
 }
